6f921918ab18fb5f2000d321d3d83d9d1e396aa2
fghrsh <fghrsh@wxw.moe>
2022-11-06 07:54:15 +0800

    reduce reset interval

--- src/worker.php
+++ src/worker.php
@@ -2,8 +2,8 @@
 
 function worker() {
     global $db, $cycle; $idle = 0; if (!isset($cycle)) $cycle = 0;
-    $pdo = $db->prepare('update `queues` set `id` = last_insert_id(id), `inuse` = 1 where `inuse` = 0 and `timestamp` <= ? order by `retry`, `timestamp` asc limit 1');
-    $pdo->execute([time()]);
+    $pdo = $db->prepare('update `queues` set `id` = last_insert_id(id), `inuse` = 1, `timestamp` = :timestamp where `inuse` = 0 and `timestamp` <= :timestamp order by `retry`, `timestamp` asc limit 1');
+    $pdo->execute([':timestamp' => time()]);
     $pdo = $db->query('select q.id, c.name as club, t.tid, t.type, t.jsonld, q.target, q.retry from `queues` as `q` left join `tasks` as `t` on q.tid = t.tid left join `clubs` as `c` on t.cid = c.cid where `id` = last_insert_id() and row_count() <> 0');
     if ($task = $pdo->fetch(PDO::FETCH_ASSOC)) {
         switch ($task['type']) {
@@ -46,13 +46,13 @@ function worker() {
     } else $idle = 1;
     if ($idle || $cycle > 9) {
         $pdo = $db->prepare('delete from `tasks` where `queues` < 1 and `timestamp` <= :timestamp');
-        $pdo->execute([':timestamp' => time() - 600]);
+        $pdo->execute([':timestamp' => time() - 30]);
         $pdo = $db->prepare('update `queues` set `inuse` = 0 where `inuse` = 1 and `timestamp` <= :timestamp');
-        $pdo->execute([':timestamp' => time() - 600]);
+        $pdo->execute([':timestamp' => time() - 30]);
         $pdo = $db->prepare('update `blacklist` set `inuse` = 0 where `inuse` = 1 and `timestamp` <= :timestamp');
-        $pdo->execute([':timestamp' => time() - 600]);
-        $pdo = $db->prepare('update `blacklist` set `id` = last_insert_id(id), `inuse` = 1 where `inuse` = 0 and `timestamp` <= ? order by `timestamp` asc limit 1');
-        $pdo->execute([time()]);
+        $pdo->execute([':timestamp' => time() - 30]);
+        $pdo = $db->prepare('update `blacklist` set `id` = last_insert_id(id), `inuse` = 1, `timestamp` = :timestamp where `inuse` = 0 and `timestamp` <= :timestamp order by `timestamp` asc limit 1');
+        $pdo->execute([':timestamp' => time()]);
         $pdo = $db->query('select `id`, `retry`, `target` from `blacklist` where `id` = last_insert_id() and row_count() <> 0');
         if ($target = $pdo->fetch(PDO::FETCH_ASSOC)) {
             Club_Exist('blacklist_target_recheck');
